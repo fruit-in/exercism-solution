c2ded0880e591e48ac0d2be33b3314288aa68e78
fruit-in <42092501+fruit-in@users.noreply.github.com>
2020-11-21 01:13:08 +0800

    + rust-spiral-matrix

---
+++ rust/spiral-matrix/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
+    let size = size as usize;
+    let mut num = 1;
+    let mut mat = vec![vec![0; size]; size];
+
+    for i in 0..((size + 1) / 2) {
+        for c in i..(size - 1 - i) {
+            mat[i][c] = num;
+            num += 1;
+        }
+        for r in i..(size - 1 - i) {
+            mat[r][size - 1 - i] = num;
+            num += 1;
+        }
+        for c in ((i + 1)..(size - i)).rev() {
+            mat[size - 1 - i][c] = num;
+            num += 1;
+        }
+        for r in ((i + 1)..(size - i)).rev() {
+            mat[r][i] = num;
+            num += 1;
+        }
+    }
+    mat[size / 2][(size - 1) / 2] = (size * size) as u32;
+
+    mat
+}
