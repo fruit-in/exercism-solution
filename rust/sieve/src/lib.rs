c4500fb337abb342cbd421441a217def09d7516e
fruit-in <42092501+fruit-in@users.noreply.github.com>
2020-12-31 18:03:01 +0800

    improve rust-sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,16 +1,10 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    if upper_bound < 2 {
-        return vec![];
-    }
-
     let mut is_prime = vec![true; upper_bound as usize + 1];
-    is_prime[0] = false;
-    is_prime[1] = false;
 
     for i in 2..=((upper_bound as f64).sqrt() as usize) {
         if is_prime[i] {
-            for j in i..=(upper_bound as usize / i) {
-                is_prime[i * j] = false;
+            for j in ((i * i)..=(upper_bound as usize)).step_by(i) {
+                is_prime[j] = false;
             }
         }
     }
