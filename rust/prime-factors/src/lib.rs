e29de2c80a1a2624658a9906d876017e6e9d5326
fruit-in <42092501+fruit-in@users.noreply.github.com>
2021-01-11 18:01:35 +0800

    improve rust-prime-factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -3,25 +3,13 @@ pub fn factors(mut n: u64) -> Vec<u64> {
     let mut ret = vec![];
 
     while n > 1 {
-        if is_prime(i) {
-            while n % i == 0 {
-                ret.push(i);
-                n /= i;
-            }
+        while n % i == 0 {
+            ret.push(i);
+            n /= i;
         }
 
         i += 1;
     }
 
     ret
-}
-
-pub fn is_prime(n: u64) -> bool {
-    let mut i = 2;
-
-    while i * i <= n && n % i != 0 {
-        i += 1;
-    }
-
-    i * i > n
 }
